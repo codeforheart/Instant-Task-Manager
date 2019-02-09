3e864c279fcb49e36a80301c781fe40d092e8472
codeforheart <47487102+codeforheart@users.noreply.github.com>
2019-02-10 01:08:16 +0200

    server port updated

--- instant-task-manager-server/server.js
+++ instant-task-manager-server/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 3003; // set port
+const port = process.env.PORT || 3333; // set port
 const server = http.createServer(app); // handle req res, should have a lstener that handles req/res
 
 server.listen(port);
